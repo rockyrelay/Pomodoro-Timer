8a016bc07a7907da06b124e44328c95504f7555e
rockyrelay <81175151+rockyrelay@users.noreply.github.com>
2023-01-02 20:25:13 +0000

    added js directions / task

---
+++ script.js
@@ -0,0 +1,16 @@
+// Pomodoro to dos
+
+// - 25m timer
+// - 5m [short break]
+// - 30m [long break]
+// - 3 rotations of work-short, then 1 rotation of work-long (repeat)
+
+// buttons
+// - start
+// - pause
+// - skip to break
+
+// show
+// - time remaining
+// - rotation
+// - buttons
